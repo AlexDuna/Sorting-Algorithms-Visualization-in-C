dc55a0ac52f6d08ec4dd8e54ae623446fe261988
Alex Duna <daniel.alex17@yahoo.com>
2026-01-23 19:19:00 +0200

    Completed the bubble sort visualisation.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -23,7 +24,8 @@ void initialize_array(int *array, int n) {
   }
 }
 
-void draw_bars(SDL_Surface *surface, int *array, int n) {
+void draw_bars(SDL_Surface *surface, int *array, int n, int highlight1,
+               int highlight2) {
   int background_color = SDL_MapRGB(surface->format, 36, 36, 36);
   SDL_FillRect(surface, NULL, background_color);
 
@@ -37,8 +39,54 @@ void draw_bars(SDL_Surface *surface, int *array, int n) {
     int y = SCREEN_HEIGHT - bar_height;
 
     SDL_Rect bar = {x, y, bar_width - 1, bar_height};
-    int bar_color = SDL_MapRGB(surface->format, 24, 251, 169);
+
+    int r, g, b;
+    if (i == highlight1) {
+      r = 204;
+      g = 0;
+      b = 0;
+    } else if (i == highlight2) {
+      r = 0;
+      g = 102;
+      b = 204;
+    } else {
+      r = 24;
+      g = 251;
+      b = 169;
+    }
+    int bar_color = SDL_MapRGB(surface->format, r, g, b);
     SDL_FillRect(surface, &bar, bar_color);
+  }
+}
+
+void swap(int *xp, int *yp) {
+  int temp = *xp;
+  *xp = *yp;
+  *yp = temp;
+}
+
+void bubble_sort_visual(SDL_Window *window, SDL_Surface *surface, int *array,
+                        int n) {
+  int i, j;
+  bool swapped;
+  for (i = 0; i < n - 1; i++) {
+    swapped = false;
+    for (j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(&array[j], &array[j + 1]);
+        swapped = true;
+      }
+      SDL_Event e;
+      while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT)
+          return;
+      }
+      draw_bars(surface, array, n, j, j + 1);
+      SDL_UpdateWindowSurface(window);
+      SDL_Delay(3);
+    }
+    if (!swapped)
+      break;
   }
 }
 
@@ -70,7 +118,12 @@ int main(int argc, char *args[]) {
       // get window surface
       screenSurface = SDL_GetWindowSurface(window);
 
-      draw_bars(screenSurface, array, NUM_BARS);
+      draw_bars(screenSurface, array, NUM_BARS, -1, -1);
+      SDL_UpdateWindowSurface(window);
+
+      bubble_sort_visual(window, screenSurface, array, NUM_BARS);
+
+      draw_bars(screenSurface, array, NUM_BARS, -1, -1);
       SDL_UpdateWindowSurface(window);
 
       // hack to get the window to stay up
